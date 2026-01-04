2d5bbdb2931aa286d742e473a875dc9dd3c32918
bada55fa7a55 <me@6d6178.com>
2026-01-04 07:18:48 +0100

    Fix header logo prerender and styled-components styles

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,7 +1,14 @@
 import { renderToString } from 'solid-js/web';
+import { extractCss } from 'solid-styled-components';
 import { WiperTool } from 'WiperTool';
 import './index.css';
 
 export function render() {
-  return renderToString(() => <WiperTool />);
+  const appHtml = renderToString(() => <WiperTool />);
+  const styles = extractCss();
+
+  return {
+    html: appHtml,
+    styles: styles ? `<style id="_goober">${styles}</style>` : '',
+  };
 }
