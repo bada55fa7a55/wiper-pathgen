0946620c1ee5ae4f52802f0cca7a73944d5f72d8
bada55fa7a55 <me@6d6178.com>
2026-01-05 21:11:12 +0100

    Refactor G-code, use G0 for non-extrusion linear moves

--- src/WiperTool/lib/gcode.ts
+++ src/WiperTool/lib/gcode.ts
@@ -8,10 +8,10 @@ export type Point3D = {
   z: number;
 };
 
-function micronsToPrinter(microns: Point, referencePoint: Point): Point {
+function relativeToAbsolute(microns: Point, referencePoint: Point): Point {
   return {
-    x: umToMm(referencePoint.x + microns.x),
-    y: umToMm(referencePoint.y + microns.y),
+    x: referencePoint.x + microns.x,
+    y: referencePoint.y + microns.y,
   };
 }
 
@@ -24,6 +24,87 @@ function formatAxisZ(value: number) {
   const rounded = Math.round(value * 100) / 100;
   return rounded.toFixed(2);
 }
+
+function formatAxisE(value: number) {
+  const rounded = Math.round(value * 100000) / 100000;
+  return rounded.toFixed(5);
+}
+
+function quotedParam(value: string, { leadingSpace = false }: { leadingSpace?: boolean } = {}): string {
+  return `${leadingSpace ? ' ' : ''}"${value}"`;
+}
+
+type GCodeParamValue = string | number | null | undefined;
+
+function formGCodeCommand(command: string, params: Record<string, GCodeParamValue> | null, comment?: string) {
+  const commandParts = [command];
+
+  if (params) {
+    for (const [param, value] of Object.entries(params)) {
+      if (value === undefined || value === null || value === '') {
+        commandParts.push(param);
+      } else {
+        commandParts.push(`${param}${value}`);
+      }
+    }
+  }
+
+  if (comment) {
+    commandParts.push(`; ${comment}`);
+  }
+
+  return commandParts.join(' ');
+}
+
+const gCodeCommands = {
+  comment(text: string) {
+    return `; ${text}`;
+  },
+
+  linearMove(coords: Partial<Point3D & { e: number }>, feedRate?: number, comment?: string) {
+    const params: Record<string, string> = {
+      ...(coords.x !== undefined ? { X: formatAxisXY(umToMm(coords.x)) } : {}),
+      ...(coords.y !== undefined ? { Y: formatAxisXY(umToMm(coords.y)) } : {}),
+      ...(coords.z !== undefined ? { Z: formatAxisZ(umToMm(coords.z)) } : {}),
+      // Note: Extrusion is in millimeter, not microns
+      ...(coords.e !== undefined ? { E: formatAxisE(coords.e) } : {}),
+      ...(feedRate !== undefined ? { F: String(feedRate) } : {}),
+    };
+
+    if (coords.e !== undefined) {
+      return formGCodeCommand('G1', params, comment);
+    }
+    return formGCodeCommand('G0', params, comment);
+  },
+
+  enableSteppers() {
+    return formGCodeCommand('M17', null, 'enable steppers');
+  },
+
+  disableSteppers() {
+    return formGCodeCommand('M84', { X: null, Y: null, E: null }, 'disable motors');
+  },
+
+  absolutePositioning() {
+    return formGCodeCommand('G90', null, 'use absolute positioning');
+  },
+
+  autoHome() {
+    return formGCodeCommand('G28', null, 'home all without mesh bed level');
+  },
+
+  // https://reprap.org/wiki/G-code#M862.3:_Model_name
+  // https://github.com/prusa3d/Prusa-Firmware-Buddy/blob/v6.4.0/src/marlin_stubs/M862_2_3.cpp
+  gCodeCompatibilityCheck(printerId: string) {
+    return formGCodeCommand('M862.3', { P: quotedParam(printerId, { leadingSpace: true }) }, 'printer model check');
+  },
+
+  // https://reprap.org/wiki/G-code#M862.6:_Firmware_features
+  // https://github.com/prusa3d/Prusa-Firmware-Buddy/blob/v6.4.0/src/marlin_stubs/M862_6.cpp
+  firmwareFeatureCheck(feature: string) {
+    return formGCodeCommand('M862.6', { P: quotedParam(feature) }, 'FW feature check');
+  },
+} satisfies Record<string, (...args: any[]) => string>;
 
 type GenerateWipingSequenceGCodeOptions = {
   wipingSequence: WipingStep[];
@@ -45,15 +126,17 @@ function generateHeaderGCodeCommands({
   padRef,
 }: GenerateHeaderGCodeOptions) {
   return [
-    `; ${printerOriginalCleaningGcode} ; <- stock nozzle cleaning`,
-    `; Start ${printerName} nozzle wiping sequence`,
-    `; REF: X${formatMicronsToMmString(padRef.x)} Y${formatMicronsToMmString(padRef.y)} Z${formatMicronsToMmString(padRef.z)}`,
-    `; GEN: ${formatDateISO(new Date())}`,
+    gCodeCommands.comment(`${printerOriginalCleaningGcode} ; <- stock nozzle cleaning`),
+    gCodeCommands.comment(`Start ${printerName} nozzle wiping sequence`),
+    gCodeCommands.comment(
+      `REF: X${formatMicronsToMmString(padRef.x)} Y${formatMicronsToMmString(padRef.y)} Z${formatMicronsToMmString(padRef.z)}`,
+    ),
+    gCodeCommands.comment(`GEN: ${formatDateISO(new Date())}`),
   ];
 }
 
 function generateFooterGCodeCommands() {
-  return ['; End nozzle wiping sequence'];
+  return [gCodeCommands.comment('End nozzle wiping sequence')];
 }
 
 const getPointsFromSequence = (sequence: WipingStep[]): Point[] =>
@@ -71,17 +154,17 @@ function generateWipingSequenceGCodeCommands({
     return [];
   }
 
-  const zHeight = umToMm(padTopRight.z - plungeDepth);
+  const zHeight = padTopRight.z - plungeDepth;
   const gCode: string[] = [];
 
   points.forEach((pt) => {
-    const coords = micronsToPrinter(pt, padTopRight);
-    gCode.push(`G1 X${formatAxisXY(coords.x)} Y${formatAxisXY(coords.y)} Z${formatAxisZ(zHeight)} F${feedRate}`);
+    const coords = relativeToAbsolute(pt, padTopRight);
+    gCode.push(gCodeCommands.linearMove(coords, feedRate));
   });
 
-  const zLiftHeight = umToMm(padTopRight.z - plungeDepth + zLift);
+  const zLiftHeight = padTopRight.z - plungeDepth + zLift;
   if (zLiftHeight !== zHeight) {
-    gCode.push(`G0 Z${formatAxisZ(zLiftHeight)} F${feedRate}`);
+    gCode.push(gCodeCommands.linearMove({ z: zLiftHeight }, feedRate, 'z-lift'));
   }
 
   return gCode;
@@ -146,17 +229,18 @@ export function generateTestGCodeCommands({
     y: 0.5 * printerMaxCoords.y,
   };
 
+  const { z: parkingZ, ...parkingXY } = printerParkingCoords;
+
   return [
     ...[
-      'M17 ; enable steppers',
-      // 'M862.1 P0.4 A0 F1 ; nozzle check',
-      `M862.3 P "${printerId}" ; printer model check`,
-      // 'M862.5 P2 ; g-code level check',
-      'M862.6 P"Input shaper" ; FW feature check',
-      'G90 ; use absolute coordinates',
-      'G28 ; home all without mesh bed level',
-      `G0 Z${formatAxisZ(umToMm(printerParkingCoords.z))} F10000 ; parking position z`,
-      `G1 X${formatAxisXY(umToMm(printerParkingCoords.x))} Y${formatAxisXY(umToMm(printerParkingCoords.y))} F4800 ; parking position x and y`,
+      gCodeCommands.comment('NOZZLE WIPER G-CODE TEST'),
+      gCodeCommands.enableSteppers(),
+      gCodeCommands.gCodeCompatibilityCheck(printerId),
+      gCodeCommands.firmwareFeatureCheck('Input shaper'),
+      gCodeCommands.absolutePositioning(),
+      gCodeCommands.autoHome(),
+      gCodeCommands.linearMove({ z: parkingZ }, 10000, 'parking position z'),
+      gCodeCommands.linearMove(parkingXY, 4800, 'parking position x and y'),
     ],
     ...generateHeaderGCodeCommands({
       printerName,
@@ -171,9 +255,6 @@ export function generateTestGCodeCommands({
       zLift,
     }),
     ...generateFooterGCodeCommands(),
-    ...[
-      `G1 X${formatAxisXY(umToMm(center.x))} Y${formatAxisXY(umToMm(center.y))} Z${formatAxisZ(printerParkingCoords.z)} F4800 ; parking position x and y`,
-      'M84 X Y E ; disable motors',
-    ],
+    ...[gCodeCommands.linearMove(center, 4800, 'move to center'), gCodeCommands.disableSteppers()],
   ];
 }
