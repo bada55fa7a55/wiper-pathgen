f5b56235d27c727ed4b0adeb58211fc9a88d2f3e
bada55fa7a55 <me@6d6178.com>
2026-01-18 14:39:29 +0100

    Refactor layout components

--- src/WiperTool/WiperTool.tsx
+++ src/WiperTool/WiperTool.tsx
@@ -1,17 +1,3 @@
-import { ShareLinkModal } from 'WiperTool/features/sharing/ShareLinkModal';
-import { ShareModal } from 'WiperTool/features/sharing/ShareModal';
-import { useGlobalFileDrop } from 'WiperTool/features/sharing/useGlobalFileDrop';
-import { useShareHashModal } from 'WiperTool/features/sharing/useShareHashModal';
-import { ManagedModals } from 'WiperTool/modals';
-import { AppModelProvider } from 'WiperTool/providers/AppModelProvider';
-import { ModalKeys } from 'WiperTool/ui/modals';
-import type { ParentProps } from 'solid-js';
-import { Toaster } from 'solid-toast';
-import { twc } from 'styles/helpers';
-import { Footer } from './Footer';
-import { ImportSharedWipingSequenceModal } from './features/sharing/ImportSharedWipingSequenceModal';
-import { ImportWipingSequenceModal } from './features/sharing/ImportWipingSequenceModal';
-import { Header } from './Header';
 import { ClaibrationSection } from './sections/CalibrationSection';
 import { DrawingSection } from './sections/DrawingSection';
 import { HardwareSetupSection } from './sections/HardwareSetupSection';
@@ -19,79 +5,18 @@ import { InstallationSection } from './sections/InstallationSection';
 import { IntroSection } from './sections/IntroSection';
 import { SettingsSection } from './sections/SettingsSection';
 import { TestingSection } from './sections/TestingSection';
-
-const ShellContainer = twc(
-  'div',
-  `
-  max-w
-  flex
-  flex-col
-  items-center
-  gap-10
-  `,
-);
-
-const Main = twc(
-  'main',
-  `
-  w-full
-  max-w-screen-xl
-  flex
-  flex-col
-  items-center
-  gap-10
-  `,
-);
-
-function Shell(props: ParentProps) {
-  useShareHashModal();
-  useGlobalFileDrop();
-
-  return <ShellContainer>{props.children}</ShellContainer>;
-}
+import { AppShell } from './ui/layout/AppShell';
 
 export function WiperTool() {
   return (
-    <AppModelProvider>
-      <Shell>
-        <Header />
-        <Main>
-          <IntroSection />
-          <HardwareSetupSection />
-          <ClaibrationSection />
-          <SettingsSection />
-          <DrawingSection />
-          <TestingSection />
-          <InstallationSection />
-        </Main>
-        <Footer />
-        <ManagedModals>
-          {(modal) => (
-            <>
-              <ShareModal
-                isOpen={modal.isModalOpen(ModalKeys.Share)}
-                onOpenShareLink={() => modal.openSubModal(ModalKeys.ShareLink)}
-                onOpenImport={() => modal.openModal(ModalKeys.ImportWipingSequence)}
-                onClose={modal.clearModals}
-              />
-              <ShareLinkModal
-                isOpen={modal.isModalOpen(ModalKeys.ShareLink)}
-                onBack={modal.isSubModal() ? modal.closeModal : undefined}
-                onClose={modal.clearModals}
-              />
-              <ImportWipingSequenceModal
-                isOpen={modal.isModalOpen(ModalKeys.ImportWipingSequence)}
-                onClose={modal.clearModals}
-              />
-              <ImportSharedWipingSequenceModal
-                isOpen={modal.isModalOpen(ModalKeys.ImportSharedWipingSequence)}
-                onClose={modal.clearModals}
-              />
-            </>
-          )}
-        </ManagedModals>
-        <Toaster />
-      </Shell>
-    </AppModelProvider>
+    <AppShell>
+      <IntroSection />
+      <HardwareSetupSection />
+      <ClaibrationSection />
+      <SettingsSection />
+      <DrawingSection />
+      <TestingSection />
+      <InstallationSection />
+    </AppShell>
   );
 }
